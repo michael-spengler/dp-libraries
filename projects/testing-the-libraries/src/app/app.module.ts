5354807d7b0e0414dedcd2642aef6bcd45c17a7f
michael-spengler <>
2019-11-26 22:37:34 +0100

    ready for deployment

--- projects/testing-the-libraries/src/app/app.module.ts
+++ projects/testing-the-libraries/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { DpImageGridModule } from 'dp-image-grid';
 
